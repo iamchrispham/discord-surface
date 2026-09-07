750eeb33c729a3360a866f89276cba9d5e630b15
Chris Pham <chrismpham@gmail.com>
2026-09-07 01:39:11 -0700

    fix: apply scoped review fixes

--- src/attachments.js
+++ src/attachments.js
@@ -1,1 +1,8 @@
-module.exports = require('../dist/attachments.js');
+'use strict';
+
+try {
+  module.exports = require('../dist/attachments.js');
+} catch (error) {
+  if (error?.code !== 'MODULE_NOT_FOUND') throw error;
+  throw new Error('discord-surface attachments build is missing; run npm run build before starting', { cause: error });
+}
